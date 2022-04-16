ab441be9437e9e52c464a540fd363412015c1eb6
Pongpak Tech <pongpaktech@gmail.com>
2022-04-15 19:51:36 -0700

    Formatting

--- keyboards/cannonkeys/obliterated75/keymaps/pongpaktech/keymap.c
+++ keyboards/cannonkeys/obliterated75/keymaps/pongpaktech/keymap.c
@@ -40,11 +40,11 @@ Layout:
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT_all(
-		 KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                    MO(1) ,
-		 KC_GRV ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,  KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_MINS, KC_EQL , KC_BSPC, KC_DEL , KC_HOME,
-		 KC_TAB ,  KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  ,  KC_T  ,  KC_Y  ,  KC_U  ,  KC_I  ,  KC_O  ,  KC_P  , KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-		KC_CAPS ,  KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,  KC_H  ,  KC_J  ,  KC_K  ,  KC_L  , KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT ,          KC_PGDN,
-		KC_LSFT , KC_LSFT,  KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_B  ,  KC_N  ,  KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,           KC_UP , KC_END ,
+		KC_ESC  , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,                    MO(1) ,
+		KC_GRV  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_DEL , KC_HOME,
+		KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+		KC_CAPS , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT ,          KC_PGDN,
+		KC_LSFT , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,           KC_UP , KC_END ,
 		KC_LCTL , KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 	),
 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		 _______,MEH(KC_1),MEH(KC_2),MEH(KC_3),MEH(KC_4),_______,_______,_______,_______, _______, _______, _______, _______, _______, _______, _______,
 		 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU,
 		 KC_NLCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLD,
-		 _______, _______, _______, _______, _______, _______,  RESET , NK_TOGG, _______, _______, _______, _______, _______,          KC_BRIU, KC_MUTE,
+		 _______, _______, _______, _______, _______, _______, RESET  , NK_TOGG, _______, _______, _______, _______, _______,          KC_BRIU, KC_MUTE,
 		 _______, GUI_TOG, RGB_MOD,                            RGB_TOG,                            RGB_VAD, RGB_VAI,    C(G(KC_LEFT)), KC_BRID, C(G(KC_RGHT))
 	),
 
@@ -102,10 +102,12 @@ const rgblight_segment_t PROGMEM nkro_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM no_gui_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 	{no_gui_led, 1, HSV_RED}
 );
+
 // Light LEDs 11 & 12 in yellow when keyboard layer 1 is active
 const rgblight_segment_t PROGMEM fn1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 	{11, 2, HSV_YELLOW}
 );
+
 // Light LED 12 in purple when keyboard layer 2 is active
 const rgblight_segment_t PROGMEM fn2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 	{12, 1, HSV_PURPLE}
